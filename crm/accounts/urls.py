c0b0c21461a5214fee2e64733bff126280faef01
unknown <robertkozak97@gmail.com>
2023-01-25 16:54:48 +0100

    REgistration and login - prepare templates

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
     path('', views.home, name="crm-home"),
     path('products/', views.products, name="crm-product"),
     path('customer/<str:pk>', views.customer, name="crm-customer"),
+    
     path('create_order/<str:pk>', views.createOrder, name="crm-create-order"),
     path('update_order/<str:pk>', views.updateOrder, name="crm-update-order"),
     path('delete_order/<str:pk>', views.deleteOrder, name="crm-delete-order"),
+
+    path('register/', views.registerPage, name="crm-register"),
+    path('login/', views.loginPage, name="crm-login"),
     
 ]

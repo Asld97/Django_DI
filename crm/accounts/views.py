7e81be42187f5706e8e33828ef8b655026461cd4
unknown <robertkozak97@gmail.com>
2023-01-25 18:55:55 +0100

    Login/Logout functionality done.

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
+# from django.contrib.auth.forms import UserCreationForm # -> Django built-in user creation form
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     customers = Customer.objects.all()
@@ -28,12 +31,39 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 def registerPage(request):
-    context = {}
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid(): # -> check for User login (similarity etc), password(if long enough, proper signs used etc)
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, f'Account was created for {user}')
+            return redirect('/login')
+    else:
+        form = CreateUserForm()
+
+    context = {'form': form}
+
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username') # -> username is get from input name tag
+        password = request.POST.get('password') # -> password is get from input name tag    
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
     context = {}
     return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('/login')
 
 def products(request):
     products = Product.objects.all()
